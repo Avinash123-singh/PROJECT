ec8a4cadbd24bdd4310c1d23300193ae0abded40
Avinash123-singh <avisingh0002@gmail.com>
2024-04-01 14:39:07 +0530

    bmi calculator

--- project2/BMI.js
+++ project2/BMI.js
@@ -8,16 +8,34 @@ form.addEventListener('submit', function (e) {
 
   if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = `please give a valid number ${height}`;
+
   }
   else if (weight === '' || weight < 0 || isNaN(weight)) {
     results.innerHTML = `please give us valid weight ${weight}`;
-  } else {
+  }
+  else {
     const BMI = (weight / ((height * height) / 10000)).toFixed(2);
 
+
+    let value = '';
+    if (BMI < 18.5) {
+      value = 'Underweight';
+    } else if (BMI >= 18.5 && BMI < 25) {
+      value = 'Normal weight';
+    } else if (BMI >= 25 && BMI < 30) {
+      value = 'Overweight';
+    } else {
+        value = 'Obese';
+      }
+
+   
     //show the result
 
-    results.innerHTML = `<span>${BMI}</span>`;
+    results.innerHTML = `<span>${BMI}</span><br><span>Weight category: ${value}</span>`;
+    
+
   }
+  
 });
 
 
